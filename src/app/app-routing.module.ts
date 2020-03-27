3bdf9f100c0c69c3c508f7da58fc8f89b7f83a8e
Juan Carlos Perez <jcarlosp1986@gmail.com>
2020-03-27 17:53:50 -0500

    Routing childs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,23 +10,9 @@ import { RegisterComponent } from './login/register.component';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent}
-      , {path: 'progress', component: ProgressComponent}
-      , {path: 'graficas1', component: Graficas1Component}
-      , {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ]
-  }
-  , {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent}
   , {path: 'register', component: RegisterComponent}
   , {path: '**', component: NopagefoundComponent}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const APP_ROUTES = RouterModule.forRoot(routes, {useHash: true});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
@@ -13,24 +12,19 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { PagesModule } from './pages/pages.module';
+import { APP_ROUTES } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
